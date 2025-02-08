c1d97bf7b96a5a0e60ceb6e86842f7e24a85ccff
Timon Dubreuil <timondubreuil@outlook.com>
2025-02-08 17:20:28 +0100

    started work on google accounts

--- adventure-map-api/src/models/User.ts
+++ adventure-map-api/src/models/User.ts
@@ -5,7 +5,8 @@ import bcrypt from 'bcryptjs';
 interface IUser extends Document {
   username: string;
   email: string;
-  password: string;
+  password: string | undefined;
+  account_type:'Password'|'Google';
   comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
@@ -13,14 +14,25 @@ interface IUser extends Document {
 const userSchema = new Schema<IUser>({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  account_type:{type:String,enum:['Password','Google'],default:'Password'},
+  password: { type: String},
 });
 
 // Hash the password before saving the user
 userSchema.pre<IUser>('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+    if(this.account_type!='Password')
+    {
+        return next()
+    }
+
+    if (!this.password){
+        return next(new Error('User has account type "Password" but no password is provided!'));
+    }
+    if (!this.isModified('password')) {
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 // Method to compare passwords
