ce485ab17f33486d28906fa27319532c8bfdd280
Timon Dubreuil <timondubreuil@outlook.com>
2025-02-10 19:12:29 +0100

    created a blank project that works

--- adventure-map-app/src/services/authService.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export const loginEmail = async (username:string,password:string) =>
-{
-    const response = await axios.post('http://localhost:3001/auth/login', { username, password })
-
-    return response.data.token
-}
