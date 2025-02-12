8776345587009ebc4b0042136250c7f136b14cd6
Timon Dubreuil <timondubreuil@outlook.com>
2025-02-12 19:13:47 +0100

    set up flutter

--- adventureMapApp/src/models/User.ts
+++
@@ -1,9 +0,0 @@
-interface IUser extends Document {
-    username: string;
-    email: string;
-    password: string | undefined;
-    account_type:'Password'|'Google';
-    comparePassword(candidatePassword: string): Promise<boolean>;
-  }
-
-export default IUser
