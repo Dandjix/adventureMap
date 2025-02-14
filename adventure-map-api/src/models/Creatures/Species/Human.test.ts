6e2fb204f584bb276abd4a260059933a3016accc
Timon Dubreuil <timondubreuil@outlook.com>
2025-02-14 22:32:04 +0100

    added rings test

--- adventure-map-api/src/models/Creatures/Species/Human.test.ts
+++ adventure-map-api/src/models/Creatures/Species/Human.test.ts
@@ -2,7 +2,6 @@ import { WorldDate } from "../../util/WorldDate";
 import { World } from "../../World/World";
 import Backpack from "../Items/Accessories/Backpack";
 import { Ring } from "../Items/Accessories/Ring";
-import { Abomination } from "./Abomination";
 import { Human } from "./Human";
 
 test("one backpack equipped, no other can be equipped", () => {
@@ -12,10 +11,32 @@ test("one backpack equipped, no other can be equipped", () => {
   )
 
 
-  const bob = new Abomination("Bob",WorldDate.now(world),world,"male")
+  const bob = new Human("Bob",WorldDate.now(world),world,"male")
   const backpack1 = new Backpack(10)
   const backpack2 = new Backpack(10)
 
   expect(bob.equip(backpack1)).toBe(true)
   expect(bob.equip(backpack2)).toBe(false)
+});
+
+test("ring equip tests", () => {
+  const world = new World(
+    "Tropica",
+    new Date()
+  )
+  const bob = new Human("Bob",WorldDate.now(world),world,"male")
+
+  const rings = []
+  for (let i = 0; i < 11; i++) {
+    rings.push(new Ring())
+  }
+
+  for (let i = 0; i < rings.length; i++) {
+    expect(bob.equip(rings[i])).toBe(i<10)//the first ten are ok, the eleventh is false
+  }
+
+  for (let i = 0; i < 5; i++) {
+    expect(bob.unEquipAccessory("left hand",0)).toBeDefined()
+  }
+  expect(bob.unEquipAccessory("left hand",0)).toBeUndefined()
 });
