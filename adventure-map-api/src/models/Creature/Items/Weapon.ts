47b817c01895455686051d9c2b26b5e586e54ec1
Timon Dubreuil <timondubreuil@outlook.com>
2025-02-16 13:12:28 +0100

    created Weapon

--- adventure-map-api/src/models/Creature/Items/Weapon.ts
+++ adventure-map-api/src/models/Creature/Items/Weapon.ts
@@ -1,8 +1,7 @@
-import { BodyPart } from "../BodyParts/BodyPart";
-import { Creature } from "../Creature";
+
 import { Item } from "./Item";
 
-export abstract class Weapon<BodyPartTypes extends BodyPart[], CreatureTypes extends Creature[]> extends Item
+export abstract class Weapon extends Item
 {
 
 }
